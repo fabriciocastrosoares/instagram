d62b46ab49bef2d2ba163c80ce3eb24058253bbe
fabriciocastrosoares <fabriciosoares27@gmail.com>
2025-03-31 18:48:30 -0300

    componentização story e post

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,77 +1,19 @@
+import Post from "./Post";
+
 export default function Posts(){
+
+  const objetoPosts = [
+    {nomeUsuario:"Roronoa", imagemUsuario : "assets/img/zoro.png", imagemPost: "assets/img/zoro-procurado.jpg", imagemCurtiu: "assets/img/ussop.png", usuarioCurtiu: "SogeKing"},
+    {nomeUsuario:"Sanji", imagemUsuario : "assets/img/sanji.png", imagemPost: "assets/img/sanji-procurado.jpg", imagemCurtiu: "assets/img/choper.png", usuarioCurtiu: "Toni Choper"}
+  ]
+
     return(
+  
+
         <div class="posts">
-          <div class="post">
-            <div class="topo">
-              <div class="usuario">
-                <img src="assets/img/zoro.png" alt="Zoro"/>
-                Roronoa
-              </div>
-              <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
 
-            <div class="conteudo">
-              <img src="assets/img/zoro-procurado.jpg" alt="gato-telefone"/>
-            </div>
-
-            <div class="fundo">
-              <div class="acoes">
-                <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
-
-              <div class="curtidas">
-                <img src="assets/img/ussop.png" alt="ussop"/>
-                <div class="texto">
-                  Curtido por <strong>SogeKing</strong> e <strong>outras 101.523 pessoas</strong>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="post">
-            <div class="topo">
-              <div class="usuario">
-                <img src="assets/img/sanji.png" alt="Sanji"/>
-                Sanji
-              </div>
-              <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
-
-            <div class="conteudo">
-              <img src="assets/img/sanji-procurado.jpg" alt="SAnji" />
-            </div>
-
-            <div class="fundo">
-              <div class="acoes">
-                <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
-
-              <div class="curtidas">
-                <img src="assets/img/choper.png" alt="choper"/>
-                <div class="texto">
-                  Curtido por <strong>Toni Choper</strong> e <strong>outras 99.159 pessoas</strong>
-                </div>
-              </div>
-            </div>
-          </div>
+          {objetoPosts.map((obj) => <Post imagem={obj.imagemUsuario} usuario={obj.nomeUsuario} imagemPost={obj.imagemPost} imagemCurtiu={obj.imagemCurtiu} usuarioCurtiu={obj.usuarioCurtiu}/>)}
+        
         </div>
     );
 }
--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,81 +1,21 @@
+import Story from "./Story";
+
 export default function Stories(){
+
+  const objetoStory = [
+    {nomeUsuario:"Roronoa", imagemUsuario : "assets/img/zoro.png"},
+    {nomeUsuario:"Nami", imagemUsuario : "assets/img/nami.png"},
+    {nomeUsuario:"Sanji", imagemUsuario : "assets/img/sanji.png"},
+    {nomeUsuario:"Nico Robin", imagemUsuario : "assets/img/robin.png"},
+    {nomeUsuario:"Toni Choper", imagemUsuario : "assets/img/choper.png"},
+    {nomeUsuario:"Frank", imagemUsuario : "assets/img/frank.png"},
+    {nomeUsuario:"SogeKing", imagemUsuario : "assets/img/ussop.png"},
+    {nomeUsuario:"Brook", imagemUsuario : "assets/img/brook.png"}
+];  
+
     return(
         <div class="stories">
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/zoro.png" alt="Zoro"/>
-            </div>
-            <div class="usuario">
-              Roronoa
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/nami.png" alt="Nami"/>
-            </div>
-            <div class="usuario">
-              Nami
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/sanji.png" alt="Sanji"/>
-            </div>
-            <div class="usuario">
-              Sanji
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/robin.png" alt="Robin"/>
-            </div>
-            <div class="usuario">
-              Nico Robin
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/choper.png" alt="Choper"/>
-            </div>
-            <div class="usuario">
-              Toni Choper
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/frank.png" alt="Frank"/>
-            </div>
-            <div class="usuario">
-              Frank
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/ussop.png" alt="Ussop"/>
-            </div>
-            <div class="usuario">
-              SogeKing
-            </div>
-          </div>
-
-          <div class="story">
-            <div class="imagem">
-              <img src="assets/img/brook.png" alt="Brook"/>
-            </div>
-            <div class="usuario">
-              Brook
-            </div>
-          </div>
-
-          <div class="setinha">
-            <ion-icon name="chevron-forward-circle"></ion-icon>
-          </div>
+          {objetoStory.map((obj) => <Story imagem={obj.imagemUsuario} usuario={obj.nomeUsuario}/>)}
         </div>
     );
 }
